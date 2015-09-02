a925b0938d88de5bd5d67ddedc7ccdf9babe44ef
Loucasse <takazyt@gmail.com>
2015-09-02 13:17:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-$('#quickReply message').after('test')
+if(location.pathname.indexOf('threads') != -1){
+		$('#QuickReply').find('.submitUnit').children('input').eq(0).before('<input class="button taigachat_bbcode xenForoSkin" style="float:right;" id="tagjs" value="Tag Staff" accesskey="t" type="button">');
+	}
