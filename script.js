59b2830579dd7b0fff71e6c2c1c7b551174cc012
Loucasse <takazyt@gmail.com>
2015-09-02 13:17:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+	String.prototype.uppercase = function() {
+		return this.charAt(0).toUpperCase() + this.slice(1);
+	}
 if(location.pathname.indexOf('threads') != -1){
 		$('#QuickReply').find('.submitUnit').children('input').eq(0).before('<input class="button taigachat_bbcode xenForoSkin" style="float:right;" id="tagjs" value="Tag Staff" accesskey="t" type="button">');
 	}
